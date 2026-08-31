dd690018dfa27c32b2b1dd7c3ee05163a6f72354
cody pekarek <codym.pekarek@hotmail.com>
2026-08-30 20:21:56 -0700

    Add die class that restricts number of sides between 1 and 20.

---
+++ Die.cs
@@ -0,0 +1,26 @@
+﻿namespace DiceRollerPracApp;
+
+/// <summary>
+/// Represents a single die.
+/// </summary>
+public class Die
+{
+    /// <summary>
+    /// contructs a new instance of die class with specified number of sides.
+    /// </summary>
+    /// <param name="numSides"></param>
+    public Die(byte numSides)
+    {
+        if(numSides == 0 || numSides > 20)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numSides), $"{nameof (numSides)} must be greater than 0 and less than 21.");
+        }
+        NumberOfSides = numSides;
+    }
+
+    /// <summary>
+    /// gets the number of sides of die
+    /// </summary>
+    public byte NumberOfSides {  get; private set; }
+}
+
